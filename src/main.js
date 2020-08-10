3782b17de231c9da9be1621fe322675c8d190af9
Sergio Vargas <segansoft@gmail.com>
2020-08-10 10:20:21 -0500

    Incluido el spinner

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router";
 
+//Import VueSpinners
+import { VueSpinners } from "@saeris/vue-spinners";
+
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Use VueSpinners
+Vue.use(VueSpinners);
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
