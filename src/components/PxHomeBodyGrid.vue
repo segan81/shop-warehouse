b94a2bc0d384e9f8ec0017ba1ef32e206ea6e1b7
Sergio Vargas <segansoft@gmail.com>
2020-08-21 23:48:52 -0500

    version carousel

--- src/components/PxHomeBodyGrid.vue
+++ src/components/PxHomeBodyGrid.vue
@@ -2,36 +2,39 @@
   <div>
     <b-card-group deck>
       <b-card
+        class="text-center"
         title="Looks"
-        img-src="https://placekitten.com/g/300/450"
+        img-src="https://placekitten.com/g/300/200"
         img-alt="Image"
         img-top
       >
-        <b-card-text>
+        <b-card-text class="text-justify">
           This is a wider card with supporting text below as a natural lead-in
           to additional content. This content is a little bit longer.
         </b-card-text>
       </b-card>
 
       <b-card
+        class="text-center"
         title="Items"
-        img-src="https://placekitten.com/g/300/450"
+        img-src="https://picsum.photos/id/0/300/200"
         img-alt="Image"
         img-top
       >
-        <b-card-text>
+        <b-card-text class="text-justify">
           This card has supporting text below as a natural lead-in to additional
           content.
         </b-card-text>
       </b-card>
 
       <b-card
+        class="text-center"
         title="Trends"
-        img-src="https://placekitten.com/g/300/450"
+        img-src="https://picsum.photos/id/237/300/200"
         img-alt="Image"
         img-top
       >
-        <b-card-text>
+        <b-card-text class="text-justify">
           This is a wider card with supporting text below as a natural lead-in
           to additional content. This card has even longer content than the
           first to show that equal height action.
